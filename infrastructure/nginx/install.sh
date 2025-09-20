7edd5b131451a6fedf22cb23685bd45108affcd5
mm <mmmdev2003@gmail.com>
2025-09-20 21:15:11 +0300

    убрал env

--- infrastructure/nginx/install.sh
+++ infrastructure/nginx/install.sh
@@ -11,4 +11,6 @@ apt install snapd
 
 snap install --classic certbot
 ln -s /snap/bin/certbot /usr/bin/certbot
+#sudo chmod -R 755 /root/kontur/kontur-system/volumes/tg-bot-api/
+#sudo chown -R www-data:www-data /root/kontur/kontur-system/volumes/tg-bot-api/
 #certbot --nginx
