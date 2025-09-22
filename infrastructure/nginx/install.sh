4cfb5b435bb01a54ab45b2e4f7ede11f8b19a7cf
mm <mmmdev2003@gmail.com>
2025-09-22 14:45:27 +0300

    алерт в тг бота на билд

--- infrastructure/nginx/install.sh
+++ infrastructure/nginx/install.sh
@@ -11,6 +11,4 @@ apt install snapd
 
 snap install --classic certbot
 ln -s /snap/bin/certbot /usr/bin/certbot
-#sudo chmod -R 755 /root/kontur/kontur-system/volumes/tg-bot-api/
-#sudo chown -R www-data:www-data /root/kontur/kontur-system/volumes/tg-bot-api/
 #certbot --nginx
