936788a9ba9eaf9e90e587ae0013ca246cc790fa
mm <mmmdev2003@gmail.com>
2025-09-27 12:44:23 +0300

    dev

--- infrastructure/nginx/install.sh
+++ infrastructure/nginx/install.sh
@@ -2,12 +2,10 @@
 
 apt install nginx -y
 
-export $(grep -v '^#' .env | xargs)
-cp infrastructure/nginx/nginx.conf > /etc/nginx/nginx.conf
+cp infrastructure/nginx/nginx.conf /etc/nginx/nginx.conf
 
 systemctl restart nginx
 apt install snapd
-
 
 snap install --classic certbot
 ln -s /snap/bin/certbot /usr/bin/certbot
